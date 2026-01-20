d1438c26f0d086220fb9e0e5c0a10765391ca7c6
Adenawell Edwarlys Valentin Torres <adenawell_valentin@ucne.edu.do>
2026-01-20 19:04:23 -0400

    Agregar Contexto del registro de asignaturas

--- Models/Asignaturas.cs
+++ Models/Asignaturas.cs
@@ -1,27 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Registro_Estudiante.Models
+namespace Registro_Asignaturas.Models;
+
+public class Asignaturas
 {
-    public class Asignaturas
-    {
-        [Key]
+    [Key]
 
-        public int AsinaturaId { get; set; }
-        
+    public int AsinaturaId { get; set; }
+    
 
-        [Required(ErrorMessage = "El codigo es obligaritorio")]
-        public int Codigo { get; set; }
+    [Required(ErrorMessage = "El codigo es obligaritorio")]
+    public int Codigo { get; set; }
 
-        [Required(ErrorMessage = "")]
-        public string Nombre { get; set; } = string.Empty;
+    [Required(ErrorMessage = "")]
+    public string Nombre { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "La aula de la asignatura es obligatoria")]
-        public string Aula{ get; set; }
+    [Required(ErrorMessage = "La aula de la asignatura es obligatoria")]
+    public string Aula{ get; set; }
 
-        [Required]
-        [Range(1, 4, ErrorMessage = "Los creditos deben estar entre 1 y 4")]
-        public string Creditos { get; set; }
+    [Required]
+    [Range(1, 4, ErrorMessage = "Los creditos deben estar entre 1 y 4")]
+    public string Creditos { get; set; }
 
 
-    }
 }
