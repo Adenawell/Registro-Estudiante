c8b863a5fa2da35297425710db69d2d7916a9d8b
Adenawell Edwarlys Valentin Torres <adenawell_valentin@ucne.edu.do>
2026-01-20 18:58:06 -0400

    Agregar Modelo Asignaturas

---
+++ DAL/ContextoAsignaturas.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_Asignaturas.Models;
+
+namespace Registro_Asignaturas.DAL;
+
+public class ContextoAsignaturas : DbContext
+{
+    public ContextoAsignaturas(DbContextOptions<ContextoAsignaturas> options) : base(options) { }
+    public DbSet<Asignaturas> Asignaturas { get; set; } = null!;
+
+}
---
+++ Models/Asignaturas.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registro_Asignaturas.Models;
+
+public class Asignaturas
+{
+    [Key]
+    public int AsignaturaId { get; set; }
+
+    [Required(ErrorMessage = "El código de la asignatura es obligatorio.")]
+    public int Codigo { get; set; }
+
+    [Required(ErrorMessage = "El nombre de la asignatura no puede estar vacío.")]
+    [StringLength(100, ErrorMessage = "El nombre es demasiado largo.")]
+    public string Nombre { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Debe especificar un aula.")]
+    public string Aula { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Los créditos son obligatorios.")]
+    [Range(1, 4, ErrorMessage = "Los créditos deben estar en un rango de 1 a 4.")]
+    public int Creditos { get; set; }
+}
