2c0af7b391c340260bab445c0bb370fcd60bf356
Adenawell Edwarlys Valentin Torres <adenawell_valentin@ucne.edu.do>
2026-01-31 12:42:04 -0400

    Creacion de registro tipos de puntos y sus clases service , entidad  y contexto

---
+++ DAL/ContextoTiposPuntos.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_Estudiante.DAL;
+using Registro_Estudiante.Models;
+using Registro_Puntos.Models;
+
+namespace Registro_Puntos.DAL
+{
+    public class ContextoTiposPuntos : DbContext
+    {
+        public ContextoTiposPuntos(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<TiposPuntos> TiposPuntos { get; set; } = null!;
+    }
+}
---
+++ Models/TiposPuntos.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Registro_Puntos.Models;
+
+public class TiposPuntos
+{
+    [Key]
+    public int TipoId { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    public string Nombre { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "La descripción es obligatoria")]
+    public string Descripcion { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El valor de puntos es obligatorio")]
+    public int? ValorPuntos { get; set; }
+
+    [Required(ErrorMessage = "El color es obligatorio")]
+    public string Color { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El icono es obligatorio")]
+    public string Icono { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El estado activo es obligatorio")]
+    public bool? Activo { get; set; }
+}
